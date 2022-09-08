1df08b17e969247aa0dc125d7dab3195d12fe82e
Felix <felixokorie01@gmail.com>
2022-09-08 15:05:03 -0700

    task 5 update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,22 @@
-/* Hello, printf*/
 #include <stdio.h>
 
-main(void)
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n);
-	return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	    return (0);
+
 }
