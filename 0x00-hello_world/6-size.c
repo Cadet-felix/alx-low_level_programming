0329dc7c5b83679272e00283920316ce02bf3339
Felix <felixokorie01@gmail.com>
2022-09-09 02:57:25 -0700

    task 6 update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,6 +4,7 @@
  * main - entry point
  *
  * Return: always 0
+ *
  */
 int main(void)
 {
@@ -19,3 +20,4 @@ int main(void)
 	printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
+}
