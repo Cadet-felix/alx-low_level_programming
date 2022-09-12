8afa39ca8a3eeece2db15b7dbf5fe61a76dfc010
Felix <felixokorie01@gmail.com>
2022-09-12 13:12:32 -0700

    advance task

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,19 +3,16 @@
 /**
  * main - assign a random number to the variable and each time it is executed
  * prints the last digit of the number 
- * Return: Always 0.
+ * Return: Always 0 (success)
  */
 int main(void)
 {
 	int n;
 
 	for (n = 0; n < 10; n++)
-	{
 		printf("%d", n);
-	}
 
 	printf("\n");
 
-	return (o);
+	return (0);
 }
-
