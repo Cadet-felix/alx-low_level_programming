45fbf7c035d8744376a84d3531b74a6f57b4d8e5
Felix <felixokorie01@gmail.com>
2022-09-13 15:52:09 -0700

    function, nest tast

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * Checks for lowercase Character.
+ *
+ * Return: 1 if c is lowercase, otherwise 0.
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
