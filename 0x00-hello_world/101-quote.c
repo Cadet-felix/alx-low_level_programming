5f7e8b3df062f103c59ce384e246f1450689034b
Felix <felixokorie01@gmail.com>
2022-09-14 05:50:10 -0700

    update task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,20 +1,21 @@
 /*
- * file: 101-quote.c
+ * File: 101-quote.c
  * Auth: Brennan D Baraban
  */
 
-#include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - print "and that piece of art is useful" -Dora Korpar, 2015-10-19",
- * fellowed by new line, to standard error.
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",*
+ *                followed by a new line, to standard error.
  *
- * Return: alway 1.
+ * Return: Always 1.
  */
 int main(void)
 {
 	write(2,
-			"and that piece of art is useful\" -Dora Korpar, 2015-9-19\n",
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 			59);
+	
 	return (1);
 }
