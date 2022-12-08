8634b85f142b8c1b3f7bc770429f46ab87d1075b
Felix <felixokorie01@gmail.com>
2022-12-08 12:04:05 -0800

    task 2

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "list.h"
+/**
+ * dlistint_len - returns number of elements in a linked list
+ * @h: head pointer to the node
+ * Returns: number
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		num++;
+		h = h->next;
+	}
+	return (num);
+}
---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * *add_dnodeint - Adds a new node at the beginning of a list
+ * @head: head pointer to the node
+ * @n: data to be add
+ * Return:  the address of the new element, or NULL if it failed
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *newnode;
+	newnode = (dlistint_t *)malloc(sizeof(dlistint_t));
+
+	if (!newnode)
+		return (NULL);
+
+	newnode->n = n;
+	newnode->prev = NULL;
+	head->prev = newnode;
+	newnode->next = head;
+	head = newnode;
+}
